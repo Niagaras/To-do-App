1fb31d0f65b70f9a1734330d64831ebead289da2
Nigar Asadova <nigar.asadova@azintelecom.az>
2026-03-01 22:55:56 +0400

    Adding edit mutation

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -3,7 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import type { Task } from "../models";
 import { useStyles } from "../styles/useStyles";
 import { queryClient } from "../main";
-import { deleteTask } from "../libs/taskApi";
+import { deleteTask, updateTask } from "../libs/taskApi";
 
 interface Props {
     task: Task;
@@ -11,10 +11,19 @@ interface Props {
 
 export default function TaskItem({task}: Props) {
     const classes = useStyles();
+
     const deleteMutation = useMutation({
       mutationFn : deleteTask,
       onSuccess: () => queryClient.invalidateQueries({queryKey: ['tasks']}),
     });
+
+    const editMutation = useMutation({
+      mutationFn : updateTask,
+      onSuccess: () => queryClient.invalidateQueries({
+        queryKey: ['tasks']
+      })
+    });
+
   return (
     <div className={classes.taskItem}>
         <h3 className={classes.taskTitle}>{task.title}</h3>
@@ -25,7 +34,7 @@ export default function TaskItem({task}: Props) {
           <button 
           style={{backgroundColor: 'red', color: 'white', padding: '5px 10px', borderRadius: '5px', cursor: 'pointer'}}
           onClick={() => deleteMutation.mutate(task.id)}>Delete</button>
-          <button style={{backgroundColor: 'yellow', padding: '5px 10px', borderRadius: '5px', cursor: 'pointer'}}>Edit</button>
+          <button style={{backgroundColor: 'yellow', padding: '5px 10px', borderRadius: '5px', cursor: 'pointer'}} onClick={() => editMutation.mutate(task)}>Edit</button>
         </div>
     </div>
   )
