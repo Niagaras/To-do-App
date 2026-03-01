778abb90a525d1d8bf5a39276b98ad145bedf795
Nigar Asadova <nigar.asadova@azintelecom.az>
2026-03-01 12:53:04 +0400

    fetch tasks

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,19 @@
 import { useStyles } from "../styles/useStyles";
 import Column from "./Column";
-
+import { fetchTasks } from "../libs/taskApi";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Board() {
     const classes = useStyles();
+    const {data : tasks = []} = useQuery({
+        queryKey: ['tasks'],
+        queryFn: fetchTasks,
+    })
   return (
     <div className={classes.container}>
-        <Column title="Todo" status="todo" tasks={[]}/>
-        <Column title="In Progress" status="in-progress" tasks={[]}/>
-        <Column title="Done" status="done" tasks={[]}/>
+        <Column title="Todo" status="todo" tasks={tasks}/>
+        <Column title="In Progress" status="in-progress" tasks={tasks}/>
+        <Column title="Done" status="done" tasks={tasks}/>
     </div>
   )
 }
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,6 +1,11 @@
+import type { Task } from "../models";
 
+interface Props {
+    task: Task;
+}
 
-export default function TaskItem({task}: {task: any}) {
+export default function TaskItem({task}: Props) {
+    
   return (
     <div>
         <h3>{task.title}</h3>
